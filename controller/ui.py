1bb10e42dec89a93eab94cf31be2dc03f63450e4
Abdala <mtnabdala@gmail.com>
2020-10-14 11:33:24 -0500

    methods for collect user's input and prints message to them

--- controller/ui.py
+++ controller/ui.py
@@ -1,1 +1,32 @@
+def message(msg):
+    """ Prints a message for the user
+     :param msg: the message to print"""
+    print(msg)
+
+
+def get_non_empty_string(question):
+    """accepts only alpha characers for an answer """
+    answer = input(question)
+    while True:
+        if answer.isalpha() == False:
+            message("Please use alpha characters only!") 
+            answer = input(question)
+        else:
+            return answer
+
+
+def get_id():
+    """ Ask for ID, validate to ensure is positive integer
+    :returns: the ID value """
+    while True:
+        try:
+            id = int(input('Enter art work ID: '))
+            if id > 0:
+                return id
+            else:
+                message('Please enter a positive number.')
+
+        except ValueError:
+            message('Please enter a number.')
+
 
